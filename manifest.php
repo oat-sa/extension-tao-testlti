c6df872f0c03ef90c97d629957c85eb9834dc9f0
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-01-22 14:48:17 +0000

    added license support to manifest
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/lti/ltiTestConsumer/trunk@18025 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -29,7 +29,8 @@ $taopath = dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'tao'.DIRECTORY_SEPARA
 return array(
 	'name' => 'ltiTestConsumer',
 	'description' => 'LTI Test Consumer',
-	'version' => '0.8',
+    'license' => 'GPL-2.0',
+    'version' => '0.8',
 	'author' => 'Open Assessment Technologies',
 	'dependencies' => array('taoTests', 'taoLti'),
 	'models' => array(
