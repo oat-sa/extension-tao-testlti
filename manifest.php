7256fc5764a4f4f92c18e23ae40ffa0df29f63d4
Aleh Hutnikau <hutnikau@1pt.com>
2018-07-27 16:33:09 +0300

    TAO-6542_bump_version

--- manifest.php
+++ manifest.php
@@ -31,7 +31,7 @@ return array(
 	'label' => 'LTI Test Consumer',
 	'description' => 'An experimental LTI test consumer which allows the embedding of external content as a test withing Tao. Does not support outcomes.',
     'license' => 'GPL-2.0',
-    'version' => '1.1.1',
+    'version' => '1.1.2',
 	'author' => 'Open Assessment Technologies',
 	'requires' => array(
 	    'taoTests' => '>=6.5.1',
